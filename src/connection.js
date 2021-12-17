8d0b0e9397b65e5aff6fa3c986bb3933b03521e4
franzea <fzg0001@alu.medac.es>
2021-12-17 14:22:13 +0100

    17-12-2021

--- src/connection.js
+++ src/connection.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
   database: "proyectophp",
-  port:"82"
+  password: "",
+  port:"3307"
 });
 
 con.connect(function(err) {
@@ -13,9 +13,18 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
-let sql = "SELECT * from usuarios where ID_Usuario = 15";
-con.query(sql, function (err, result) {
-  if (err) throw err;
 
-  console.log("Result: " + JSON.stringify(result,null,2));
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res)
+{
+    let sql = "SELECT * from usuarios where ID_Usuario = 15";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+    
+      console.log("Result: " + JSON.stringify(result,null,2));
+    });
 });
+
+var server=app.listen(5000,function() {});

19247726d6c42ad7db5410c8c924a3d91a0450d3
franzea <fzg0001@alu.medac.es>
2021-12-20 11:50:23 +0100

    20-12-2021

--- src/connection.js
+++ src/connection.js
@@ -9,6 +9,7 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
+
   if (err) throw err;
   console.log("Connected!");
 });
@@ -17,14 +18,41 @@ con.connect(function(err) {
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res)
-{
-    let sql = "SELECT * from usuarios where ID_Usuario = 15";
+/*app.get('/',function(req,res){
+
+    let sql = "SELECT * from usuarios";
     con.query(sql, function (err, result) {
       if (err) throw err;
     
       console.log("Result: " + JSON.stringify(result,null,2));
     });
+});*/
+
+app.get('/usuario',function(req,res){
+
+    const id = req.query.id_usuario;
+    const sql = "SELECT * from usuarios where ID_Usuario = "+id;
+
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+
+      console.log("Result: " + JSON.stringify(result,null,2));
+
+      res.json(result);
+    });
 });
 
-var server=app.listen(5000,function() {});
+app.post('/editUsuario',function(req,res){
+  
+  const id_usu = req.body.id_usu
+  const name = req.body.nombre
+  const email = req.body.email
+
+  const sql = "update usuarios set nombre="+name+"set dni="+email
+
+  con.query(sql,function() {
+    
+  })
+})
+
+var server = app.listen(5000,function(err,re) {});

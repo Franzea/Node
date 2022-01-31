74bfcc109181b499a77fc3f78b264452a39f3cb5
franzea <fzg0001@alu.medac.es>
2022-01-31 10:15:29 +0100

    31-1-2022

--- src/P1-T2/p1-t2.js
+++ src/P1-T2/p1-t2.js
@@ -172,10 +172,134 @@ app.post('/eliminar_usuario', function (req,res) {
 
 
 
-//
+//Modificar datos de un vehículo
+app.post('/modificar_vehiculo',function(req,res){
+
+  const id = req.body.id_vehiculo;
+  const matricula = req.body.matricula;
+  const marca = req.body.marca;
+  const modelo = req.body.modelo;
+  const fabricacion = req.body.fabricacion;
+
+  const sql = "UPDATE vehiculos set  Matricula = '"+matricula+"', Marca ='"+marca+"', Modelo ='"+modelo+"', Año_fabricacion = "+fabricacion+" where ID_Vehiculo = "+id;
+
+  datos.con.query(sql, function (err, result) {
+    if (err) throw res.json({Estado: "ERROR", Error: "Alguno de los datos a modificar no es correcto"});
+
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json({Estado: "OK"});
+  });
+});
+
+
+
+//Crear vehículo
+app.post('/crear_vehiculo',function(req,res){
+
+  const matricula = req.body.matricula;
+  const id_usu = req.body.id_usu;
+  const marca = req.body.marca;
+  const modelo = req.body.modelo;
+  const fabricacion = req.body.fabricacion;
+
+  const sql = "INSERT INTO vehiculos(Matricula, Id_usuario, Marca, Modelo, Año_fabricacion) VALUES ('"+matricula+"', "+id_usu+", '"+marca+"',  '"+modelo+"', "+fabricacion+")" ;
+  console.log(sql);
+  datos.con.query(sql, function (err, result) {
+    if (err) {
+      console.log(err.message)
+      res.json({Estado: "ERROR", Error: err.message});
+    }
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json({Estado: "OK"});
+  });
+});
+
+
+
+//Eliminar vehiculo
+app.post('/eliminar_vehiculo', function (req,res) {
+
+  const id = req.body.id_vehiculo;
+
+  const sql = "delete from vehiculos where ID_Vehiculo = "+id;
+
+  datos.con.query(sql, function (err, result) {
+    if (err) throw res.json({Estado: "ERROR", Error: "Alguno dato no es correcto"});
+
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json({Estado: "OK" });
+  });
+});
+
+
+
+//Modificar datos de un servicio
+app.post('/modificar_servicio',function(req,res){
+
+  const id = req.body.id_servicio;
+  const id_vehi = req.body.id_vehi;
+  const nombre = req.body.nombre;
+  const precio = req.body.precio;
+  const tipo = req.body.tipo;
+  const descripcion = req.body.descripcion;
+  const fecha = req.body.fecha;
+
+  const sql = "UPDATE servicios set  ID_vehiculo = "+id_vehi+", Nombre = '"+nombre+"', Precio ="+precio+", tipo_servicio ='"+tipo+"', descripcion ='"+descripcion+"', fecha = '"+fecha+"' where ID_Servicio = "+id;
+
+  datos.con.query(sql, function (err, result) {
+    if (err) throw res.json({Estado: "ERROR", Error: "Alguno de los datos a modificar no es correcto"});
+
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json({Estado: "OK"});
+  });
+});
 
 
 
 //
+//Crear servicio
+app.post('/crear_servicio',function(req,res){
+
+  const id_vehi = req.body.id_vehi;
+  const nombre = req.body.nombre;
+  const precio = req.body.precio;
+  const tipo = req.body.tipo;
+  const descripcion = req.body.descripcion;
+  const fecha = req.body.fecha;
+
+  const sql = "INSERT INTO servicios(ID_vehiculo, Nombre, Precio, tipo_servicio, descripcion, fecha) VALUES ("+id_vehi+", '"+nombre+"', "+precio+", '"+tipo+"', '"+descripcion+"', '"+fecha+"')" ;
+  console.log(sql);
+  datos.con.query(sql, function (err, result) {
+    if (err) {
+      console.log(err.message)
+      res.json({Estado: "ERROR", Error: err.message});
+    }
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json({Estado: "OK"});
+  });
+});
+
+
+
+//Eliminar servicio
+app.post('/eliminar_servicio', function (req,res) {
+
+  const id = req.body.id_servicio;
+
+  const sql = "delete from servicios where ID_Servicio = "+id;
+
+  datos.con.query(sql, function (err, result) {
+    if (err) throw res.json({Estado: "ERROR", Error: "Alguno dato no es correcto"});
+
+    console.log("Result: " + JSON.stringify(result,null,2));
+
+    res.json({Estado: "OK" });
+  });
+});
 
 var server = app.listen(5000,function(err,re) {});
